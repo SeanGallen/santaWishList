a14a01b6fddfd21bb6bb5648cac1d836eb45991a
Sean <sjgallen@gmail.com>
2019-08-10 14:11:14 -0500

    working tests for buttons

--- santaFrontEnd/src/app/app-routing.module.ts
+++ santaFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WishListMainPageComponent } from './components/wish-list-main-page/wish-list-main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WishListMainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- santaFrontEnd/src/app/components/wish-list-main-page/wish-list-main-page.component.spec.ts
+++ santaFrontEnd/src/app/components/wish-list-main-page/wish-list-main-page.component.spec.ts
@@ -22,4 +22,34 @@ describe('WishListMainPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have Four Buttons', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    let countOfButtons = compiled.querySelectorAll('button');
+    expect(countOfButtons.length).toBe(4);
+  });
+
+  it('should have a write button', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button').textContent).toContain('Write'); 
+  });
+
+  it('should have a update button', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    let countOfButtons = compiled.querySelectorAll('button');
+    expect(countOfButtons[1].textContent).toBe("Update");
+  });
+
+  it('should have a show all button', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    let countOfButtons = compiled.querySelectorAll('button');
+    expect(countOfButtons[2].textContent).toBe("Show All");
+  });
+
+  it('should have a delete button', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    let countOfButtons = compiled.querySelectorAll('button');
+    expect(countOfButtons[3].textContent).toBe("Delete");
+  });
+
 });
